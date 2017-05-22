931a0cf7e1e31e6b9f8d8268ab113b741d14b608
Dominika Zduniewicz <dominikazduniewicz@gmail.com>
2017-05-22 09:51:30 +0200

    dodałam tablice z książkami, całość podzielona na strony wraz z przyciskiem pierwsza, poprzednia, następna, ostatnia. Na jednej stronie wyświetlają się 54 książki a pasek stanu to 25 stron.

--- app/controllers/books.controller.js
+++ app/controllers/books.controller.js
@@ -1,6 +1,23 @@
 app.controller('BooksController', ['books', 'loader', function (books, loader) {
   var vm = this;
 
+  vm.originalBooks = angular.copy(books);
   vm.books = books;
+  vm.currentPage = 0;
+  vm.itemsPerPage = 54;
+  vm.showPageNumber = 25;
+  vm.currentItems = [];
   vm.loader = loader;
+
+  vm.setItemsOnCurrentPage = function () {
+    var startAt = Math.max((vm.currentPage - 1) * vm.itemsPerPage, 0);
+    var endAt = Math.max(vm.currentPage, 1) * vm.itemsPerPage;
+    vm.currentItems = vm.books.slice(startAt, endAt);
+  };
+
+  vm.pageChanged = function () { // została zmieniona strona z książkami
+    vm.setItemsOnCurrentPage(); // ustawiamy tablicę z książkami na aktualnej stronie
+  }
+
+  vm.setItemsOnCurrentPage(); // ustawiamy tablicę z książkami na aktualnej stronie
 }]);
