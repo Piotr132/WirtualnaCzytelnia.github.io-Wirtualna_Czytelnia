3e9c5e11f37d50301ea442123a28c5c7776bde7f
Adrian Szypuła <kermittt@onet.eu>
2017-05-24 17:45:25 +0200

    dodałem możliwość wyszukiwania książek, domyślnie ustawiłem wyszukiwanie od 3 znaków w górę - można to zmienić w sekcji books.controllers.js

--- app/controllers/books.controller.js
+++ app/controllers/books.controller.js
@@ -7,6 +7,7 @@ app.controller('BooksController', ['books', 'loader', function (books, loader) {
   vm.itemsPerPage = 54;
   vm.showPageNumber = 25;
   vm.currentItems = [];
+  vm.searchPhrase = '';
   vm.loader = loader;
 
   vm.setItemsOnCurrentPage = function () {
@@ -17,6 +18,23 @@ app.controller('BooksController', ['books', 'loader', function (books, loader) {
 
   vm.pageChanged = function () { // została zmieniona strona z książkami
     vm.setItemsOnCurrentPage(); // ustawiamy tablicę z książkami na aktualnej stronie
+  }
+
+  vm.searchPhraseChanged = function () {  // zmienił się tekst w wyszukiwarce
+    vm.books = angular.copy(vm.originalBooks);  // kopiujemy tablicę z książkami, która została zwrócona z api, aby nie trzymać referencji, a utworzyć nową tablicę
+    loader.enableLoader();  // pokazujemy loader
+
+    if (vm.searchPhrase.length >= 3) {  // tylko jeśli długość wpisanego słowa jest większa lub równa 3
+      var tmp = vm.books.filter(function (el) { // odrzucamy książki, których ani tytuł ani autor nie pasują do wpisanego słowa, wielkość liter nie ma znaczenia
+        return el.author.toLowerCase().indexOf(vm.searchPhrase.toLowerCase()) !== -1
+        || el.title.toLowerCase().indexOf(vm.searchPhrase.toLowerCase()) !== -1;
+      });
+
+      vm.books = tmp;
+    }
+
+    vm.setItemsOnCurrentPage(); // ustawiamy tablicę z książkami na aktualnej stronie
+    loader.disableLoader();  // ukrywamy loader
   }
 
   vm.setItemsOnCurrentPage(); // ustawiamy tablicę z książkami na aktualnej stronie
--- app/services/api.service.js
+++ app/services/api.service.js
@@ -1,4 +1,4 @@
-app.service('api', ['config', '$http', 'loader', function (config, $http, loader) {
+﻿app.service('api', ['config', '$http', 'loader', function (config, $http, loader) {
   var vm = this;
   vm.apiUrl = config.apiUrl;
   vm.loader = loader;
@@ -6,16 +6,6 @@ app.service('api', ['config', '$http', 'loader', function (config, $http, loader
   vm.getBooks = function () {
     vm.loader.enableLoader(); // pokazujemy loader
     return $http.get(vm.apiUrl + 'books/').then( // wykonujemy zapytanie do api
-      function (response) {
-        vm.loader.disableLoader(); // ukrywamy loader
-        return response.data; // zwracamy treść, którą zwróciło api
-      }
-    );
-  };
-
-  vm.getBookInfo = function (bookName) {
-    vm.loader.enableLoader(); // pokazujemy loader
-    return $http.get(vm.apiUrl + 'books/' + bookName).then( // wykonujemy zapytanie do api
       function (response) {
         vm.loader.disableLoader(); // ukrywamy loader
         return response.data; // zwracamy treść, którą zwróciło api
